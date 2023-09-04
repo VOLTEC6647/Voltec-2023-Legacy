6977e7610ca158d606f05ff23ab9f4edca04a4e5
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-03 23:01:31 -0600

    Increased Constants

--- src/main/java/com/team6647/util/Constants.java
+++ src/main/java/com/team6647/util/Constants.java
@@ -29,7 +29,7 @@ public class Constants {
                 public static final int leftMotorID = 15;
                 public static final int rightMotorID = 16;
 
-                public static final double elevatorKp = 0;
+                public static final double elevatorKp = 0.3;
                 public static final double elevatorKi = 0;
                 public static final double elevatorKd = 0;
 
@@ -49,19 +49,25 @@ public class Constants {
                 public static final int pivotIntakeID = 13;
                 public static final int intakeMotorID = 14;
 
-                public static final double intakeKp = 0.065; //0.05
-                public static final double intakeKi = 0;
-                public static final double intakeKd = 0;//1.3;
+                public static final double intakeKp = 0.02; //0.059 //0.05
+                public static final double intakeKi = 0;//0.0001;
+                public static final double intakeKd = 0.0;//1.3;
 
                 public static final double minIntakePosition = 8;
                 public static final double maxIntakePosition = 90;
 
-                public static final double intakeHomedPosition = 28;
-                public static final double intakeExtendedPosition = 68;
+                public static final double intakeHomedPosition = 32;
+                public static final double intakeExtendedPosition = 65;
 
                 public static final double pivotPositionConversionFactor = 100;
                 public static final double pivotZeroOffset = 90.0467753;
 
                 public static final double intakeSpeed = 0.5;
+        }
+
+        public static class IndexerConstants{
+                public static final int indexerMotorID = 17;
+
+                public static final double indexerSpeed = 0.2;
         }
 }
