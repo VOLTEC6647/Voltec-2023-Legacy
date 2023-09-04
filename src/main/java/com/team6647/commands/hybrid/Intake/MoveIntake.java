31d4d37e1fcc29e3d4e5b2f9b5c90f3d6431eb46
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-03 23:01:09 -0600

    Cleaned MoveIntake

--- src/main/java/com/team6647/commands/hybrid/Intake/MoveIntake.java
+++ src/main/java/com/team6647/commands/hybrid/Intake/MoveIntake.java
@@ -12,32 +12,21 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class MoveIntake extends CommandBase {
   private IntakeSubsystem intakeSubsystem;
   private RollerState rollerState;
-  /** Creates a new MoveIntake. */
+
   public MoveIntake(IntakeSubsystem intakeSubsystem, RollerState rollerState) {
     this.intakeSubsystem = intakeSubsystem;
     this.rollerState = rollerState;
     addRequirements(intakeSubsystem);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     intakeSubsystem.changeRollerState(rollerState);
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     intakeSubsystem.changeRollerState(RollerState.STOPPED);
   }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
 }
