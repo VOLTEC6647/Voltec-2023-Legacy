73cbcd3e1c19f142c54593c2b8464416159e2ba2
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-03 23:02:20 -0600

    Created autonomous paths

---
+++ src/main/java/com/team6647/commands/auto/AutonomousPaths.java
@@ -0,0 +1,11 @@
+/**
+ * Written by Juan Pablo Gutiérrez
+ * 03 09 2023
+ */
+
+package com.team6647.commands.auto;
+
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+public class AutonomousPaths {
+}
