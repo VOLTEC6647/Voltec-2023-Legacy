a987a9ae9c9ac5cc80347968ab1eacf34a89d981
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-03 23:02:01 -0600

    Created pivotCone subsystem

---
+++ src/main/java/com/team6647/subsystems/PivotConeSubsystem.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.team6647.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PivotConeSubsystem extends SubsystemBase {
+  /** Creates a new PivotConeSubsystem. */
+  public PivotConeSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
