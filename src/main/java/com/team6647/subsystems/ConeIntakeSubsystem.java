ed7d5c5ea0611ade1c3b0dd615e325ceec03a6a8
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-03 23:14:50 -0600

    Finished coneIntakeSubsystem

--- src/main/java/com/team6647/subsystems/ConeIntakeSubsystem.java
+++ src/main/java/com/team6647/subsystems/ConeIntakeSubsystem.java
@@ -5,14 +5,60 @@
 
 package com.team6647.subsystems;
 
+import com.andromedalib.motorControllers.SuperSparkMax;
+import com.andromedalib.motorControllers.IdleManager.GlobalIdleMode;
+import com.team6647.subsystems.IntakeSubsystem.RollerState;
+import com.team6647.util.Constants.ConeIntakeConstants;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ConeIntakeSubsystem extends SubsystemBase {
+  private static ConeIntakeSubsystem instance;
+
+  private SuperSparkMax intakeMotor = new SuperSparkMax(ConeIntakeConstants.intakeMotorID, GlobalIdleMode.Coast, false,
+      80);
+
+  private RollerState mState = RollerState.STOPPED;
+
   /** Creates a new ConeIntakeSubsystem. */
-  public ConeIntakeSubsystem() {}
+  private ConeIntakeSubsystem() {
+  }
+
+  public static ConeIntakeSubsystem getInstance() {
+    if (instance == null) {
+      instance = new ConeIntakeSubsystem();
+    }
+    return instance;
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
+
+  public void changeRollerState(RollerState rollerState) {
+    switch (rollerState) {
+      case STOPPED:
+        mState = RollerState.STOPPED;
+        setIntakeSpeed(0);
+        break;
+      case COLLECTING:
+        mState = RollerState.COLLECTING;
+        setIntakeSpeed(ConeIntakeConstants.intakeSpeed);
+        break;
+      case SPITTING:
+        mState = RollerState.SPITTING;
+        setIntakeSpeed(-ConeIntakeConstants.intakeSpeed);
+        break;
+    }
+  }
+
+  private void setIntakeSpeed(double speed) {
+    intakeMotor.set(speed);
+  }
+
+  /* Telemetry */
+  public RollerState getRollerState() {
+    return mState;
+  }
+
 }
