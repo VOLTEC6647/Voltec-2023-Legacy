00f50aff51b013c8c6a989b555a92db3523f1947
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-03 22:59:28 -0600

    Added PathPLannerServer

--- src/main/java/com/team6647/Robot.java
+++ src/main/java/com/team6647/Robot.java
@@ -1,6 +1,7 @@
 package com.team6647;
 
 import com.andromedalib.robot.SuperRobot;
+import com.pathplanner.lib.server.PathPlannerServer;
 import com.team6647.util.shuffleboard.TelemetryManager;
 
 public class Robot extends SuperRobot {
@@ -13,5 +14,7 @@ public class Robot extends SuperRobot {
         super.setRobotContainer(container, TelemetryManager.getInstance(), false);
 
         super.robotInit();
+
+        PathPlannerServer.startServer(5811); 
     } 
 }
