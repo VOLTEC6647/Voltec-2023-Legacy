96d1a293bb112dc4c328977373e9af200ddda3ed
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-21 08:04:24 -0600

    Removed DebugTab

--- src/main/java/com/team6647/util/shuffleboard/tabs/DebugTab.java
+++ src/main/java/com/team6647/util/shuffleboard/tabs/DebugTab.java
@@ -6,35 +6,17 @@
 package com.team6647.util.shuffleboard.tabs;
 
 import com.andromedalib.shuffleboard.ShuffleboardTabBase;
-import com.team6647.subsystems.IntakeSubsystem;
-import com.team6647.subsystems.PivotCubeSubsystem;
 
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 public class DebugTab extends ShuffleboardTabBase {
 
-    private PivotCubeSubsystem cubeintakeSubsystem = PivotCubeSubsystem.getInstance();
-    private IntakeSubsystem intakeSubsystem = IntakeSubsystem.getInstance();
-
-    private GenericEntry cubeIntakePivotPosition;
-    private GenericEntry cubeIntakePivotState;
-    private GenericEntry cubeRollerState;
-
     public DebugTab(ShuffleboardTab tab) {
 
-        cubeIntakePivotPosition = tab.add("Cube Intake Position", cubeintakeSubsystem.getPivotPosition())
-                .withWidget(BuiltInWidgets.kAccelerometer).getEntry();
-        cubeIntakePivotState = tab.add("Cube Pivot State", cubeintakeSubsystem.getPivotState().toString()).getEntry();
-        cubeRollerState = tab.add("Cube Roller State", intakeSubsystem.getRollerState().toString()).getEntry();
     }
 
     @Override
     public void updateTelemetry() {
-        cubeIntakePivotPosition.setDouble(cubeintakeSubsystem.getPivotPosition());
-        cubeIntakePivotState.setString(cubeintakeSubsystem.getPivotState().toString());
-        cubeRollerState.setString(intakeSubsystem.getRollerState().toString());
     }
 
 }
