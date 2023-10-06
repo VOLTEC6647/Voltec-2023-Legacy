b58b17becb47c036621dc68036348400479ce13e
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-10-06 09:24:39 -0600

    Finished ArmIntakeSubsystem

--- src/main/java/com/team6647/subsystems/ArmIntakeSubsytem.java
+++ src/main/java/com/team6647/subsystems/ArmIntakeSubsytem.java
@@ -9,19 +9,32 @@ import com.andromedalib.motorControllers.SuperTalonFX;
 import com.andromedalib.motorControllers.IdleManager.GlobalIdleMode;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.team6647.subsystems.IntakeSubsystem.RollerState;
-import com.team6647.util.Constants.ConeIntakeConstants;
+import com.team6647.util.Constants.ArmIntakeConstants;
 
+import edu.wpi.first.networktables.BooleanEntry;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StringEntry;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmIntakeSubsytem extends SubsystemBase {
   private static ArmIntakeSubsytem instance;
+  private static NetworkTable armIntakeTable;
+  private static StringEntry ArmIntakeStateEntry;
+  private static BooleanEntry beamBrakeEntry;
 
-  private SuperTalonFX intakeMotor = new SuperTalonFX(ConeIntakeConstants.intakeMotorID, GlobalIdleMode.Coast, false);
+  private SuperTalonFX intakeMotor = new SuperTalonFX(ArmIntakeConstants.intakeMotorID, GlobalIdleMode.Coast, false);
 
   private RollerState mState = RollerState.STOPPED;
+  private DigitalInput beamBrake = new DigitalInput(ArmIntakeConstants.beamBrakePort);
 
   /** Creates a new ConeIntakeSubsystem. */
   private ArmIntakeSubsytem() {
+
+    armIntakeTable = NetworkTableInstance.getDefault().getTable("ArmTable/Intake");
+    ArmIntakeStateEntry = armIntakeTable.getStringTopic("ArmIntakeState").getEntry(getRollerState().toString());
+    beamBrakeEntry = armIntakeTable.getBooleanTopic("BeamBrake").getEntry(getBeamBrake());
   }
 
   public static ArmIntakeSubsytem getInstance() {
@@ -33,6 +46,7 @@ public class ArmIntakeSubsytem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    updateNT();
   }
 
   public void changeRollerState(RollerState rollerState) {
@@ -43,11 +57,11 @@ public class ArmIntakeSubsytem extends SubsystemBase {
         break;
       case COLLECTING:
         mState = RollerState.COLLECTING;
-        setIntakeSpeed(ConeIntakeConstants.intakeSpeed);
+        setIntakeSpeed(ArmIntakeConstants.intakeSpeed);
         break;
       case SPITTING:
         mState = RollerState.SPITTING;
-        setIntakeSpeed(-ConeIntakeConstants.intakeSpeed);
+        setIntakeSpeed(-ArmIntakeConstants.intakeSpeed);
         break;
     }
   }
@@ -57,8 +71,17 @@ public class ArmIntakeSubsytem extends SubsystemBase {
   }
 
   /* Telemetry */
+  private void updateNT() {
+    ArmIntakeStateEntry.set(getRollerState().toString());
+    beamBrakeEntry.set(getBeamBrake());
+  }
+
   public RollerState getRollerState() {
     return mState;
+  }
+
+  public boolean getBeamBrake() {
+    return beamBrake.get();
   }
 
 }
