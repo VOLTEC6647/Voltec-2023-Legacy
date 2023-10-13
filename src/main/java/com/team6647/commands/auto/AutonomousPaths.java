93c1f1df2fb0ae906ded0683e9693b6d913a932d
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-10-13 13:57:44 -0600

    Created autonomousPaths

--- src/main/java/com/team6647/commands/auto/AutonomousPaths.java
+++ src/main/java/com/team6647/commands/auto/AutonomousPaths.java
@@ -5,5 +5,33 @@
 
 package com.team6647.commands.auto;
 
-public class AutonomousPaths {
+import com.andromedalib.andromedaSwerve.commands.SwerveDriveCommand;
+import com.andromedalib.andromedaSwerve.systems.AndromedaSwerve;
+import com.team6647.util.AutoUtils;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class AutonomousPaths extends AutoUtils {
+
+    public static Command topAuto() {
+        return Commands.sequence(autoDriveSubsystem.getFullAuto("Top"));
+    }
+
+    public static Command middleAuto() {
+        return Commands.sequence(new SwerveDriveCommand(AndromedaSwerve.getInstance(null, null), () -> 0.0, () -> -0.5,
+                () -> 0, () -> false).withTimeout(3),
+                new SwerveDriveCommand(AndromedaSwerve.getInstance(null, null), () -> 0.0, () -> 0.5,
+                        () -> 0, () -> false).withTimeout(2),
+                new AutoBalance(AndromedaSwerve.getInstance(null, null), autoDriveSubsystem));
+    }
+
+    public static Command bottomAuto() {
+        return Commands.sequence(autoDriveSubsystem.getFullAuto("Bottom"));
+    }
+
+    public static Command leaveCommunityAuto() {
+        return Commands.sequence(getGridPlacement(Piece.Cone), new SwerveDriveCommand(
+                AndromedaSwerve.getInstance(null, null), () -> 0.0, () -> 0.5, () -> 0.0, () -> false));
+    }
 }
