890d2a9fa1876a51eb9c1da5757396b8e5e556cb
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-10-13 13:58:38 -0600

    Added auto to robot container

--- src/main/java/com/team6647/RobotContainer.java
+++ src/main/java/com/team6647/RobotContainer.java
@@ -24,115 +24,137 @@ import com.team6647.subsystems.ElevatorSubsystem.ElevatorPositionState;
 import com.team6647.subsystems.IntakeSubsystem.RollerState;
 import com.team6647.util.AutoUtils;
 import com.team6647.util.Constants.OperatorConstants;
+import com.team6647.util.shuffleboard.TelemetryManager;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer extends SuperRobotContainer {
-    private static RobotContainer instance;
+        private static RobotContainer instance;
 
-    /* Systems */
-    private AndromedaSwerve andromedaSwerve;
-    private AutoDriveSubsystem autoDriveSubsystem;
-    private IntakePivotSubsystem cubeintakeSubsystem;
-    private ElevatorSubsystem elevatorSubsystem;
-    private ArmIntakeSubsytem armIntakeSubsystem;
-    private ArmPivotSubsystem armPivotSubsystem;
+        /* Systems */
+        private AndromedaSwerve andromedaSwerve;
+        private AutoDriveSubsystem autoDriveSubsystem;
+        private IntakePivotSubsystem cubeintakeSubsystem;
+        private ElevatorSubsystem elevatorSubsystem;
+        private ArmIntakeSubsytem armIntakeSubsystem;
+        private ArmPivotSubsystem armPivotSubsystem;
 
-    private RobotContainer() {
-    }
-
-    public static RobotContainer getInstance() {
-        if (instance == null) {
-            instance = new RobotContainer();
+        private RobotContainer() {
         }
 
-        return instance;
-    }
+        public static RobotContainer getInstance() {
+                if (instance == null) {
+                        instance = new RobotContainer();
+                }
 
-    @Override
-    public void initSubsystems() {
-        andromedaSwerve = AndromedaSwerve.getInstance(new FalconAndromedaModule[] {
-                new FalconAndromedaModule(0, "Front Right Module", AndromedaMap.mod1Const,
-                        AndromedaProfileConfig.getConfig(AndromedaProfiles.ANDROMEDA_CONFIG)),
-                new FalconAndromedaModule(1, "Back Right Module", AndromedaMap.mod2Const,
-                        AndromedaProfileConfig.getConfig(AndromedaProfiles.ANDROMEDA_CONFIG)),
-                new FalconAndromedaModule(2, "Back Left Module", AndromedaMap.mod3Const,
-                        AndromedaProfileConfig.getConfig(AndromedaProfiles.ANDROMEDA_CONFIG)),
-                new FalconAndromedaModule(3, "Front Left Module", AndromedaMap.mod4Const,
-                        AndromedaProfileConfig.getConfig(AndromedaProfiles.ANDROMEDA_CONFIG)), },
-                AndromedaProfileConfig.getConfig(AndromedaProfiles.ANDROMEDA_CONFIG));
+                return instance;
+        }
 
-        autoDriveSubsystem = AutoDriveSubsystem.getInstance(andromedaSwerve);
-        cubeintakeSubsystem = IntakePivotSubsystem.getInstance();
-        elevatorSubsystem = ElevatorSubsystem.getInstance();
-        armIntakeSubsystem = ArmIntakeSubsytem.getInstance();
-        armPivotSubsystem = ArmPivotSubsystem.getInstance();
-    }
+        @Override
+        public void initSubsystems() {
+                andromedaSwerve = AndromedaSwerve.getInstance(new FalconAndromedaModule[] {
+                                new FalconAndromedaModule(0, "Front Right Module", AndromedaMap.mod1Const,
+                                                AndromedaProfileConfig.getConfig(AndromedaProfiles.ANDROMEDA_CONFIG)),
+                                new FalconAndromedaModule(1, "Back Right Module", AndromedaMap.mod2Const,
+                                                AndromedaProfileConfig.getConfig(AndromedaProfiles.ANDROMEDA_CONFIG)),
+                                new FalconAndromedaModule(2, "Back Left Module", AndromedaMap.mod3Const,
+                                                AndromedaProfileConfig.getConfig(AndromedaProfiles.ANDROMEDA_CONFIG)),
+                                new FalconAndromedaModule(3, "Front Left Module", AndromedaMap.mod4Const,
+                                                AndromedaProfileConfig
+                                                                .getConfig(AndromedaProfiles.ANDROMEDA_CONFIG)), },
+                                AndromedaProfileConfig.getConfig(AndromedaProfiles.ANDROMEDA_CONFIG));
 
-    @Override
-    public void configureBindings() {
+                autoDriveSubsystem = AutoDriveSubsystem.getInstance(andromedaSwerve);
+                cubeintakeSubsystem = IntakePivotSubsystem.getInstance();
+                elevatorSubsystem = ElevatorSubsystem.getInstance();
+                armIntakeSubsystem = ArmIntakeSubsytem.getInstance();
+                armPivotSubsystem = ArmPivotSubsystem.getInstance();
+        }
 
-        /* Driver Controller 1 */
-        andromedaSwerve.setDefaultCommand(
-                new SwerveDriveCommand(
-                        andromedaSwerve,
-                        () -> -OperatorConstants.driverController1.getLeftX(),
-                        () -> -OperatorConstants.driverController1.getLeftY(),
-                        () -> -OperatorConstants.driverController1.getRightX(),
-                        () -> OperatorConstants.driverController1.leftStick().getAsBoolean()));
+        @Override
+        public void configureBindings() {
 
-        /* Driver Controller 2 */
+                /* Driver Controller 1 */
+                andromedaSwerve.setDefaultCommand(
+                                new SwerveDriveCommand(
+                                                andromedaSwerve,
+                                                () -> -OperatorConstants.driverController1.getLeftX(),
+                                                () -> -OperatorConstants.driverController1.getLeftY(),
+                                                () -> -OperatorConstants.driverController1.getRightX(),
+                                                () -> OperatorConstants.driverController1.leftStick().getAsBoolean()));
 
-        /* Testing Bidings */
+                /* Driver Controller 2 */
 
-        OperatorConstants.driverController2.povLeft()
-                .whileTrue(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.BOTTOM))
-                .and(OperatorConstants.driverController2.a()
-                        .whileTrue(Commands.parallel(new MoveArm(armPivotSubsystem, ArmPivotState.PLACING),
-                                new MoveArmIntake(armIntakeSubsystem, RollerState.SPITTING))))
-                .onFalse(Commands.parallel(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.HOMED),
-                        new MoveArm(armPivotSubsystem, ArmPivotState.HOMED)));
-        OperatorConstants.driverController2.povRight()
-                .whileTrue(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.MID))
-                .and(OperatorConstants.driverController2.a()
-                        .whileTrue(Commands.parallel(new MoveArm(armPivotSubsystem, ArmPivotState.PLACING),
-                                new MoveArmIntake(armIntakeSubsystem, RollerState.SPITTING))))
-                .onFalse(Commands.parallel(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.HOMED),
-                        new MoveArm(armPivotSubsystem, ArmPivotState.HOMED)));
-        OperatorConstants.driverController2.povUp()
-                .whileTrue(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.MAX))
-                .and(OperatorConstants.driverController2.a()
-                        .whileTrue(Commands.parallel(new MoveArm(armPivotSubsystem, ArmPivotState.PLACING),
-                                new MoveArmIntake(armIntakeSubsystem, RollerState.SPITTING))))
-                .onFalse(Commands.parallel(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.HOMED),
-                        new MoveArm(armPivotSubsystem, ArmPivotState.HOMED)));
+                /* Testing Bidings */
 
-        OperatorConstants.driverController2.povDown()
-                .whileTrue(Commands.parallel(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.MAX),
-                        new MoveArmIntake(armIntakeSubsystem, RollerState.COLLECTING)))
-                .onFalse(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.HOMED));
+                OperatorConstants.driverController2.povLeft()
+                                .whileTrue(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.BOTTOM))
+                                .and(OperatorConstants.driverController2.a()
+                                                .whileTrue(Commands.parallel(
+                                                                new MoveArm(armPivotSubsystem, ArmPivotState.PLACING),
+                                                                new MoveArmIntake(armIntakeSubsystem,
+                                                                                RollerState.SPITTING))))
+                                .onFalse(Commands.parallel(
+                                                new ExtendElevator(elevatorSubsystem, ElevatorPositionState.HOMED),
+                                                new MoveArm(armPivotSubsystem, ArmPivotState.HOMED)));
+                OperatorConstants.driverController2.povRight()
+                                .whileTrue(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.MID))
+                                .and(OperatorConstants.driverController2.a()
+                                                .whileTrue(Commands.parallel(
+                                                                new MoveArm(armPivotSubsystem, ArmPivotState.PLACING),
+                                                                new MoveArmIntake(armIntakeSubsystem,
+                                                                                RollerState.SPITTING))))
+                                .onFalse(Commands.parallel(
+                                                new ExtendElevator(elevatorSubsystem, ElevatorPositionState.HOMED),
+                                                new MoveArm(armPivotSubsystem, ArmPivotState.HOMED)));
+                OperatorConstants.driverController2.povUp()
+                                .whileTrue(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.MAX))
+                                .and(OperatorConstants.driverController2.a()
+                                                .whileTrue(Commands.parallel(
+                                                                new MoveArm(armPivotSubsystem, ArmPivotState.PLACING),
+                                                                new MoveArmIntake(armIntakeSubsystem,
+                                                                                RollerState.SPITTING))))
+                                .onFalse(Commands.parallel(
+                                                new ExtendElevator(elevatorSubsystem, ElevatorPositionState.HOMED),
+                                                new MoveArm(armPivotSubsystem, ArmPivotState.HOMED)));
 
-        OperatorConstants.driverController2.leftTrigger()
-                .whileTrue(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.HOMED));
+                OperatorConstants.driverController2.povDown()
+                                .whileTrue(Commands.parallel(
+                                                new ExtendElevator(elevatorSubsystem, ElevatorPositionState.MAX),
+                                                new MoveArmIntake(armIntakeSubsystem, RollerState.COLLECTING)))
+                                .onFalse(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.HOMED));
 
-        OperatorConstants.driverController2.x().whileTrue(new ToggleIntake(cubeintakeSubsystem))
-                .and(OperatorConstants.driverController2.leftTrigger()
-                        .whileTrue(AutoUtils.intakePieceSequence(RollerState.COLLECTING))
-                        .or(OperatorConstants.driverController2
-                                .rightTrigger().whileTrue(AutoUtils.intakePieceSequence(RollerState.SPITTING))))
-                .onFalse(Commands.parallel(new MoveArm(armPivotSubsystem, ArmPivotState.HOMED),
-                        new ToggleIntake(cubeintakeSubsystem)));
+                OperatorConstants.driverController2.leftTrigger()
+                                .whileTrue(new ExtendElevator(elevatorSubsystem, ElevatorPositionState.HOMED));
 
-    }
+                OperatorConstants.driverController2.x()
+                                .whileTrue(Commands.sequence(new ToggleIntake(cubeintakeSubsystem),
+                                                Commands.waitSeconds(0.3),
+                                                new MoveArm(armPivotSubsystem, ArmPivotState.INDEXING)))
+                                .and(OperatorConstants.driverController2.leftTrigger()
+                                                .whileTrue(AutoUtils.intakeCubeSequence(RollerState.COLLECTING))
+                                                .or(OperatorConstants.driverController2
+                                                                .rightTrigger()
+                                                                .whileTrue(AutoUtils.intakeCubeSequence(
+                                                                                RollerState.SPITTING))))
+                                .onFalse(Commands.parallel(new MoveArm(armPivotSubsystem, ArmPivotState.HOMED),
+                                                new ToggleIntake(cubeintakeSubsystem)));
 
-    public Command getAutonomousCommand() {
-        return autoDriveSubsystem.createFullAuto("Top");
+                OperatorConstants.driverController2.rightTrigger()
+                                .onTrue(AutoUtils.intakeConeSequence())
+                                .onFalse(new MoveArm(armPivotSubsystem, ArmPivotState.HOMED));
 
-        // PathPlannerTrajectory examplePath = PathPlanner.loadPath("Top", new
-        // PathConstraints(2, 2));
-        // return autoDriveSubsystem.followTrajectoryCommand(examplePath, true);
+        }
 
-    }
+        public Command getAutonomousCommand() {
+                try {
+                        TelemetryManager.getInstance().setEventMap();
+                        return AutoUtils.getAuto();
+                } catch (Exception e) {
+                        return Commands.waitSeconds(0.1);
+                }
+
+        }
 
 }
